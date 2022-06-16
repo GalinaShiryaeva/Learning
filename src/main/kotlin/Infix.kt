c76b8cfe8f94b89a85a8f2240c3450d9cf39c288
GalinaShiryaeva <Galla1777.35@gmail.com>
2022-06-16 11:55:24 +0300

    infix

---
+++ src/main/kotlin/Infix.kt
@@ -0,0 +1,11 @@
+data class Location(val lat: Double, val long: Double)
+infix fun Double.x(that: Double) = Location(this, that)
+operator fun Location.plus(other: Location) = Location(
+    lat = lat + other.lat, long = long + other.long
+)
+
+fun main() {
+    val position: Location = 55.7522 x 37.61557
+    val result: Location = position + Location(1.0, 1.0)
+    println(result)
+}
